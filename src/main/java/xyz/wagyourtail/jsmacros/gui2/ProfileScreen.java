42baee71d2b58caece5dbecef064290ee3f480fb
wagyourtail <ruwagray@gmail.com>
2020-07-06 11:49:55 -0600

    backport of mapping work.

--- src/main/java/xyz/wagyourtail/jsmacros/gui2/ProfileScreen.java
+++ src/main/java/xyz/wagyourtail/jsmacros/gui2/ProfileScreen.java
@@ -212,7 +212,6 @@ public class ProfileScreen extends Screen {
 
         // pname
         drawCenteredString(minecraft.textRenderer, jsMacros.profile.profileName, this.width * 7 / 12, 5, 0x7F7F7F);
-//        drawCenteredString(matricies, this.textRenderer, "Not Yet Implemented", this.width / 2, 50, 0xFFFFFFFF);
 
         // middle bar
         fill(this.width / 2, 22, this.width / 2 + 1, this.height - 1, 0xFFFFFFFF);
