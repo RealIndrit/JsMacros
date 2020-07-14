0a2ce5285433f995ac883bb81db4e0dd0eb42cb7
wagyourtail <ruwagray@gmail.com>
2020-07-13 20:13:52 -0600

    backport isKeyPressed

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/keybindFunctions.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/keybindFunctions.java
@@ -1,5 +1,6 @@
 package xyz.wagyourtail.jsmacros.runscript.functions;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.InputUtil.KeyCode;
@@ -13,6 +14,11 @@ public class keybindFunctions {
         } catch (Exception e) {
             return InputUtil.UNKNOWN_KEYCODE;
         }
+    }
+    
+    public boolean isKeyPressed(String keyName) {
+        KeyCode key = getKeyCode(keyName);
+        return InputUtil.isKeyPressed(MinecraftClient.getInstance().window.getHandle(), key.getKeyCode());
     }
     
     public void key(String keyName, boolean keyState) {
