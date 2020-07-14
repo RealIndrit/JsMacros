4f264d7f4a0a5e77b3f4bcc034f5dc53ce3a6fa2
wagyourtail <ruwagray@gmail.com>
2020-07-13 20:12:09 -0600

    Revert "backport isKeyPressed"
    This reverts commit a3aa8c96217c4b2523eff3bc0b5a809b5abe1e75.

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/keybindFunctions.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/keybindFunctions.java
@@ -1,6 +1,5 @@
 package xyz.wagyourtail.jsmacros.runscript.functions;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.InputUtil.KeyCode;
@@ -14,11 +13,6 @@ public class keybindFunctions {
         } catch (Exception e) {
             return InputUtil.UNKNOWN_KEYCODE;
         }
-    }
-    
-    public boolean isKeyPressed(String keyName) {
-        KeyCode key = getKeyCode(keyName);
-        return InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), key.getKeyCode());
     }
     
     public void key(String keyName, boolean keyState) {
