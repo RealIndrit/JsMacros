9fdc3e9b884c3922081b8761e36a5c7ea974d151
wagyourtail <ruwagray@gmail.com>
2020-07-19 21:38:18 -0600

    oops2.

--- src/main/java/xyz/wagyourtail/jsmacros/gui2/containers/FileChooser.java
+++ src/main/java/xyz/wagyourtail/jsmacros/gui2/containers/FileChooser.java
@@ -191,8 +191,8 @@ public class FileChooser extends OverlayContainer {
                 fill(mouseX-3, mouseY-textRenderer.fontHeight - 4, mouseX+width+3, mouseY-textRenderer.fontHeight - 3, 0x7F7F7F7F);
                 
                 // fill
-                drawString(textRenderer, b.getMessage(), mouseX, mouseY-textRenderer.fontHeight - 1, 0xFFFFFF);
                 fill(mouseX-2, mouseY-textRenderer.fontHeight - 3, mouseX+width+2, mouseY, 0xFF000000);
+                drawString(textRenderer, b.getMessage(), mouseX, mouseY-textRenderer.fontHeight - 1, 0xFFFFFF);
             }
         }
     }
