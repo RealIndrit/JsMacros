d863014608471dda5d19054d0091528e88988264
wagyourtail <ruwagray@gmail.com>
2020-07-27 00:48:07 -0600

    backport changes/additions

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/classes/Draw2D.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/classes/Draw2D.java
@@ -14,6 +14,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.wagyourtail.jsmacros.reflector.ItemStackHelper;
+import xyz.wagyourtail.jsmacros.runscript.classes.common.RenderCommon.item;
+import xyz.wagyourtail.jsmacros.runscript.classes.common.RenderCommon.rect;
+import xyz.wagyourtail.jsmacros.runscript.classes.common.RenderCommon.text;
 
 public class Draw2D extends DrawableHelper {
     public ArrayList<text> textFields = new ArrayList<>();
@@ -76,14 +79,14 @@ public class Draw2D extends DrawableHelper {
         rectFields.remove(r);
     }
     
-    public item addItem(int x, int y, String id) {
-        item i = new item(y, y, id);
+    public item addItem(int x, int y, String id, boolean overlay) {
+        item i = new item(y, y, id, overlay);
         itemFields.add(i);
         return i;
     }
     
-    public item addItem(int x, int y, ItemStackHelper item) {
-        item i = new item(y, y, item);
+    public item addItem(int x, int y, ItemStackHelper item, boolean overlay) {
+        item i = new item(x, y, item, overlay);
         itemFields.add(i);
         return i;
     }
@@ -94,6 +97,8 @@ public class Draw2D extends DrawableHelper {
     
     public void init() {
         textFields.clear();
+        rectFields.clear();
+        itemFields.clear();
         if (onInit != null) {
             try {
                 onInit.accept(this);
@@ -122,142 +127,20 @@ public class Draw2D extends DrawableHelper {
         } catch(Exception e) {
             return;
         }
-        
         RenderSystem.pushMatrix();
         for (rect r : rectFields) {
             r.render();
         }
         RenderSystem.popMatrix();
+        RenderSystem.pushMatrix();
         for (item i : itemFields) {
             i.render();
         }
+        RenderSystem.popMatrix();
         RenderSystem.pushMatrix();
         for (text t : textFields) {
             t.render();
         }
         RenderSystem.popMatrix();
-    }
-    
-    public static class item {
-        public int x;
-        public int y;
-        public ItemStack item;
-        
-        public item(int x, int y, String id) {
-            this.x = x;
-            this.y = y;
-            this.setItem(id, 1);
-        }
-        
-        public item(int x, int y, ItemStackHelper i) {
-            this.x = x;
-            this.y = y;
-            this.item = i.getRaw();
-        }
-        
-        public void setPos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        
-        public void setItem(ItemStackHelper i) {
-            this.item = i.getRaw();
-        }
-        
-        public void setItem(String id, int count) {
-            Item it = (Item)Registry.ITEM.get(new Identifier(id));
-            if (it != null) this.item = new ItemStack(it, count);
-        }
-        
-        public void render() {
-            MinecraftClient mc = MinecraftClient.getInstance();
-            if (item != null) {
-                ItemRenderer i = mc.getItemRenderer();
-                i.renderGuiItemIcon(item, x, y);
-                i.renderGuiItemOverlay(mc.textRenderer, item, x, y);
-            }
-        }
-    }
-    
-    public static class rect {
-        public int x1;
-        public int y1;
-        public int x2;
-        public int y2;
-        public int color;
-        
-        public rect(int x1, int y1, int x2, int y2, int color) {
-            setPos(x1, y1, x2, y2);
-            setColor(color);
-        }
-        
-        public rect(int x1, int y1, int x2, int y2, int color, int alpha) {
-            setPos(x1, y1, x2, y2);
-            setColor(color, alpha);
-        }
-        
-        public void setColor(int color) {
-            if (color <= 0xFFFFFF) color = color | 0xFF000000;
-            this.color = color;
-        }
-        
-        public void setColor(int color, int alpha) {
-            this.color = color | (alpha << 24);
-        }
-        
-        public void setAlpha(int alpha) {
-            this.color = (color & 0xFFFFFF) | (alpha << 24);
-        }
-        
-        public void setPos(int x1, int y1, int x2, int y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-        
-        public void render() {
-            Draw2D.fill(x1, y1, x2, y2, color);
-        }
-    }
-    
-    public static class text {
-        public String text;
-        public int x;
-        public int y;
-        public int color;
-        public int width;
-        public boolean shadow;
-        
-        public text(String text, int x, int y, int color, boolean shadow) {
-            MinecraftClient mc = MinecraftClient.getInstance();
-            this.text = text;
-            this.x = x;
-            this.y = y;
-            this.color = color;
-            this.width = mc.textRenderer.getStringWidth(text);
-            this.shadow = shadow;
-        }
-        
-        public void setPos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        
-        public void setText(String text) {
-            MinecraftClient mc = MinecraftClient.getInstance();
-            this.text = text;
-            this.width = mc.textRenderer.getStringWidth(text);
-        }
-        
-        public int getWidth() {
-            return this.width;
-        }
-        
-        public void render() {
-            MinecraftClient mc = MinecraftClient.getInstance();
-            if (shadow) mc.textRenderer.drawWithShadow(text, x, y, color);
-            else mc.textRenderer.draw(text, x, y, color);
-        }
     }
 }
