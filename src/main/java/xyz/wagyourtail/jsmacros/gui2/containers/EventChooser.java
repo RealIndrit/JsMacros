cc69a1a89ce1f0023f827c3722a911fba2efafa8
wagyourtail <ruwagray@gmail.com>
2020-07-27 23:25:34 -0600

    sort eventchooser

--- src/main/java/xyz/wagyourtail/jsmacros/gui2/containers/EventChooser.java
+++ src/main/java/xyz/wagyourtail/jsmacros/gui2/containers/EventChooser.java
@@ -1,6 +1,7 @@
 package xyz.wagyourtail.jsmacros.gui2.containers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.function.Consumer;
 
 import net.minecraft.client.font.TextRenderer;
@@ -55,7 +56,10 @@ public class EventChooser extends OverlayContainer {
                 this.close();
             }
         }));
-        for (String e : Profile.registry.events) {
+        
+        ArrayList<String> events = new ArrayList<>(Profile.registry.events);
+        Collections.sort(events);
+        for (String e : events) {
             addEvent(e);
         }
         this.selectEvent(selected);
